1bcb25914ac6c3174c29bb2c120115e42f9fa39a
orian <77785778+OrianP@users.noreply.github.com>
2022-02-15 18:11:47 +0000

    Move model.js into database folder
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>
    Co-authored-by: Oli <78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

---
+++ database/model.js
@@ -0,0 +1,18 @@
+import { db } from "./connection";
+
+// export function getProductData() {
+//   const SELECT_PRODUCTS = `SELECT * FROM products`;
+//   return db.query(SELECT_PRODUCTS).then((result) => {
+//     console.log(result.rows);
+//     return result.rows;
+//   });
+// }
+
+// GET POSTS //
+// function getPosts() {
+//   const SELECT_POSTS = `SELECT * FROM posts`;
+//   return db.query(SELECT_POSTS).then((result) => {
+//     // console.log(result.rows);
+//     return result.rows;
+//   });
+// }
