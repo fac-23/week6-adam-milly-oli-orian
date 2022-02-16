c5209e6e22d239db90c7ff0f492c7ccb24dce8d1
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-02-16 16:46:23 +0000

    Add sort by price
    Co-Authored-By: MillyP (she/her) <palmermilly9@gmail.com>
    Co-Authored-By: Adam <31065348+adam8-9@users.noreply.github.com>
    Co-Authored-By: Orian Peled <77785778+OrianP@users.noreply.github.com>

--- components/ProductCards.js
+++ components/ProductCards.js
@@ -1,18 +1,32 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function ProductCards({ allProductData }) {
+export default function ProductCards({ allProductData, sortOrder }) {
+  function compareNumbers(a, b) {
+    switch (sortOrder) {
+      case "highToLow":
+        return b.price - a.price;
+      case "lowToHigh":
+        return a.price - b.price;
+      case "default":
+        return 0;
+      default:
+        return 0;
+    }
+  }
   return (
     <div className={styles.grid}>
-      {allProductData.map(({ id, name, price, description, url }) => (
-        <li key={id}>
-          <Image src={url} height={300} width={300} alt="cupcake" />
-          <p>{name}</p>
-          <p>{description}</p>
-          <p>{price}</p>
-          <button>Add to basket</button>
-        </li>
-      ))}
+      {allProductData
+        .sort(compareNumbers)
+        .map(({ id, name, price, description, url }) => (
+          <li key={id}>
+            <Image src={url} height={300} width={300} alt="cupcake" />
+            <p>{name}</p>
+            <p>{description}</p>
+            <p>{price}</p>
+            <button>Add to basket</button>
+          </li>
+        ))}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
+import React from "react";
 import ProductCards from "../components/ProductCards";
+import SortControls from "../components/SortControls";
 import { getProductData } from "../database/model";
 import styles from "../styles/Home.module.css";
 
@@ -17,6 +19,8 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ allProductData }) {
+  const [sortOrder, setSortOrder] = React.useState("default");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -39,75 +43,11 @@ export default function Home({ allProductData }) {
         <p className={styles.description}>Feeling peckish?</p>
 
         <div>
-          <ProductCards allProductData={allProductData} />
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <SortControls
+            setSortOrder={setSortOrder}
+            sortOrder={sortOrder}
+          ></SortControls>
+          <ProductCards allProductData={allProductData} sortOrder={sortOrder} />
         </div>
       </main>
 
