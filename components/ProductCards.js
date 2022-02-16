e115f26cd55853e34ecd86e7770a5c71be58fba3
MillyP <millypalmer1@hotmail.co.uk>
2022-02-16 17:58:33 +0000

    Basket.js file created, added multiple items to local storage in ProductCards component
    Co-authored-by: Orian <77785778+OrianP@users.noreply.github.com>

--- components/ProductCards.js
+++ components/ProductCards.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import react from "react";
 
 export default function ProductCards({ allProductData }) {
+  const [basket, setBasket] = useState([]);
   return (
     <div className={styles.grid}>
       {allProductData.map(({ id, name, price, description, url }) => (
@@ -10,9 +13,22 @@ export default function ProductCards({ allProductData }) {
           <p>{name}</p>
           <p>{description}</p>
           <p>{price}</p>
-          <button>Add to basket</button>
+          <button
+            id={id}
+            onClick={() => {
+              if (typeof window !== "undefined") {
+                setBasket((basket) => [...basket, id]);
+                window.localStorage.setItem("Basket", basket);
+              }
+            }}
+          >
+            {console.log(id)}
+            Add to basket
+          </button>
         </li>
       ))}
     </div>
   );
 }
+
+// function addToBasket() {}
---
+++ pages/basket.js
@@ -0,0 +1,4 @@
+// Create basket component
+// Return li of every element in the basket
+// onClick handler function needs to be added to Add to Basket button
+//
