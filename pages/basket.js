f29e0c65a5afce415b912d2b855cbc13a5c3bd17
orian <77785778+OrianP@users.noreply.github.com>
2022-02-17 14:47:28 +0000

    Retrieve and display items from local storage
    Add delete items button
    
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>

--- pages/basket.js
+++ pages/basket.js
@@ -1,10 +1,10 @@
 // Create basket component
 // Return li of every element in the basket
 // onClick handler function needs to be added to Add to Basket button
-//
 
-import ProductCards from "../components/ProductCards";
-import { useEffect } from "react";
+// import ProductCards from "../components/ProductCards";
+// import { useEffect } from "react";
+import Image from "next/image";
 
 // export async function getServerSideProps() {
 //   const basketCakes = await getBasket();
@@ -17,12 +17,30 @@ import { useEffect } from "react";
 // Pass them into ProductCards component
 export default function Basket() {
   let basketItems = [];
-  useEffect(() => {
+  if (typeof window !== "undefined") {
     basketItems = JSON.parse(localStorage.getItem("Basket"));
-    // return firstValue || "";
-    console.log(basketItems);
-  }, []);
-  console.log("Basket", basketItems);
-  //   return <ProductCards allProductData={allProductData} />;
-  return <div>Hello</div>;
+  }
+  console.log("basketItems", basketItems);
+
+  return (
+    <div>
+      {basketItems.map(({ id, name, price, description, url: url }) => (
+        <li key={id}>
+          <Image src={url} height={300} width={300} alt="cupcake" />
+          <p>{name}</p>
+          <p>{description}</p>
+          <p>{price}</p>
+          {/* add onclick to delete element */}
+          <button>Remove from basket</button>
+        </li>
+      ))}
+    </div>
+  );
 }
+
+// useEffect(() => {
+//   let basketItems = [];
+//   basketItems = JSON.parse(localStorage.getItem("Basket"));
+//   // return firstValue || "";
+//   console.log(basketItems);
+// }, []);
