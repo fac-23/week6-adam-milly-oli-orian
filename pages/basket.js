cf8954fecd39d490b1b44f80d4a8cd0139f4fcbd
orian <77785778+OrianP@users.noreply.github.com>
2022-02-18 12:31:11 +0000

    Lowercase 'basket' key retrieval in local storage

--- pages/basket.js
+++ pages/basket.js
@@ -21,42 +21,40 @@ import styles from "../styles/Home.module.css";
 export default function Basket() {
   let basketItems = [];
   if (typeof window !== "undefined") {
-    basketItems = JSON.parse(localStorage.getItem("Basket"));
+    basketItems = JSON.parse(localStorage.getItem("basket"));
   }
   console.log("basketItems", basketItems);
 
   return (
-    <div className={styles.basket}>
+    <ul className={styles.basket}>
       {!basketItems ? (
-        <div className="flex-column">
+        <li className="flex-column">
           <p>Your basket is empty :( </p>
           <p>Go back and fill it with yummy cupcakes!</p>
           <Link href={"/"}>
             <a>⏎ Back to home</a>
           </Link>
-        </div>
+        </li>
       ) : (
         basketItems.map(({ id, name, price, description, url }) => (
-          <ul key={id}>
-            <li className={styles.list}>
-              <h1 className={styles.title}>Basket</h1>
-              <Image
-                className={styles.productImage}
-                src={url}
-                height={150}
-                width={150}
-                alt="cupcake"
-              />
-              <p>{name}</p>
-              <p>{description}</p>
-              <p>{price}</p>
-              <Link href={"/"}>
-                <a>⏎ Back to home</a>
-              </Link>
-            </li>
-          </ul>
+          <li className={styles.list} key={id}>
+            <h1 className={styles.title}>Basket</h1>
+            <Image
+              className={styles.productImage}
+              src={url}
+              height={150}
+              width={150}
+              alt="cupcake"
+            />
+            <p>{name}</p>
+            <p>{description}</p>
+            <p>{price}</p>
+            <Link href={"/"}>
+              <a>⏎ Back to home</a>
+            </Link>
+          </li>
         ))
       )}
-    </div>
+    </ul>
   );
 }
